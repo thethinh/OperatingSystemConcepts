5c1b577e5474f917a50d9ed061392d2e6c571cdd
ThinhNguyen <nguyenthethinh16011998@gmail.com>
2019-09-09 21:46:57 +0700

    complete homework :D

---
+++ BTL_HĐH/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace BTL_HĐH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.txtSoCot = new System.Windows.Forms.TextBox();
+            this.txtSoHang = new System.Windows.Forms.TextBox();
+            this.lblSoCot = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblSoHang = new System.Windows.Forms.Label();
+            this.insertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nhấnVàoĐâyNếuMuốnNhậpMaTrậnTheoÝMìnhToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoátToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtK = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtSoCot
+            // 
+            this.txtSoCot.Location = new System.Drawing.Point(111, 72);
+            this.txtSoCot.Name = "txtSoCot";
+            this.txtSoCot.Size = new System.Drawing.Size(100, 25);
+            this.txtSoCot.TabIndex = 7;
+            // 
+            // txtSoHang
+            // 
+            this.txtSoHang.Location = new System.Drawing.Point(111, 27);
+            this.txtSoHang.Name = "txtSoHang";
+            this.txtSoHang.Size = new System.Drawing.Size(100, 25);
+            this.txtSoHang.TabIndex = 6;
+            // 
+            // lblSoCot
+            // 
+            this.lblSoCot.AutoSize = true;
+            this.lblSoCot.Location = new System.Drawing.Point(36, 72);
+            this.lblSoCot.Name = "lblSoCot";
+            this.lblSoCot.Size = new System.Drawing.Size(51, 19);
+            this.lblSoCot.TabIndex = 1;
+            this.lblSoCot.Text = "Số cột";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.txtSoCot);
+            this.groupBox1.Controls.Add(this.txtSoHang);
+            this.groupBox1.Controls.Add(this.lblSoCot);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.lblSoHang);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(43, 51);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(262, 155);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input matrix";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.progressBar1.Location = new System.Drawing.Point(95, 114);
+            this.progressBar1.Maximum = 0;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(152, 17);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(21, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Input";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblSoHang
+            // 
+            this.lblSoHang.AutoSize = true;
+            this.lblSoHang.Location = new System.Drawing.Point(33, 30);
+            this.lblSoHang.Name = "lblSoHang";
+            this.lblSoHang.Size = new System.Drawing.Size(63, 19);
+            this.lblSoHang.TabIndex = 0;
+            this.lblSoHang.Text = "Số hàng";
+            // 
+            // insertToolStripMenuItem
+            // 
+            this.insertToolStripMenuItem.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.insertToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nhấnVàoĐâyNếuMuốnNhậpMaTrậnTheoÝMìnhToolStripMenuItem,
+            this.thoátToolStripMenuItem});
+            this.insertToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.insertToolStripMenuItem.Name = "insertToolStripMenuItem";
+            this.insertToolStripMenuItem.Size = new System.Drawing.Size(58, 23);
+            this.insertToolStripMenuItem.Text = "Insert";
+            // 
+            // nhấnVàoĐâyNếuMuốnNhậpMaTrậnTheoÝMìnhToolStripMenuItem
+            // 
+            this.nhấnVàoĐâyNếuMuốnNhậpMaTrậnTheoÝMìnhToolStripMenuItem.Image = global::BTL_HĐH.Properties.Resources.qr_code;
+            this.nhấnVàoĐâyNếuMuốnNhậpMaTrậnTheoÝMìnhToolStripMenuItem.Name = "nhấnVàoĐâyNếuMuốnNhậpMaTrậnTheoÝMìnhToolStripMenuItem";
+            this.nhấnVàoĐâyNếuMuốnNhậpMaTrậnTheoÝMìnhToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.nhấnVàoĐâyNếuMuốnNhậpMaTrậnTheoÝMìnhToolStripMenuItem.Text = "Thêm matrix";
+            // 
+            // thoátToolStripMenuItem
+            // 
+            this.thoátToolStripMenuItem.Image = global::BTL_HĐH.Properties.Resources.Close_icon;
+            this.thoátToolStripMenuItem.Name = "thoátToolStripMenuItem";
+            this.thoátToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.thoátToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.thoátToolStripMenuItem.Text = "Thoát";
+            this.thoátToolStripMenuItem.Click += new System.EventHandler(this.thoátToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(341, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(414, 431);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(701, 478);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(75, 26);
+            this.btnThoat.TabIndex = 10;
+            this.btnThoat.Text = "Exit";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(169, 38);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(66, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtK
+            // 
+            this.txtK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtK.Location = new System.Drawing.Point(58, 38);
+            this.txtK.Name = "txtK";
+            this.txtK.Size = new System.Drawing.Size(100, 23);
+            this.txtK.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "K  = ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Controls.Add(this.btnSearch);
+            this.groupBox2.Controls.Add(this.txtK);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(43, 239);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(262, 230);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Nhập số K : ";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(58, 80);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 132);
+            this.listBox1.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.insertToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(788, 27);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 513);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Matrix";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox txtSoCot;
+        private System.Windows.Forms.TextBox txtSoHang;
+        private System.Windows.Forms.Label lblSoCot;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblSoHang;
+        private System.Windows.Forms.ToolStripMenuItem thoátToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nhấnVàoĐâyNếuMuốnNhậpMaTrậnTheoÝMìnhToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem insertToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ BTL_HĐH/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace BTL_HĐH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessBar()
+        {
+            try
+            {
+                int n = int.Parse(txtSoHang.Text);
+                int m = int.Parse(txtSoCot.Text);
+                for (int i = 1; i <= (n * m); ++i)
+                {
+                    progressBar1.Invoke(new MethodInvoker(() =>
+                    {
+                        progressBar1.Maximum = (n * m);
+                        progressBar1.Value = (i);
+                    }));
+                }              
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void inputMatrix()
+        {
+            try
+            {
+                Thread pro = new Thread(new ThreadStart(ProcessBar));
+                pro.Start();
+                int[,] arr = new int[10000, 10000];
+                using (StreamWriter sw = new StreamWriter("mydata.txt"))
+                {
+                    StreamWriter sw1 = new StreamWriter("mydata2.txt");
+                    Random ran = new Random();
+                    for (int i = 1; i <= int.Parse(txtSoHang.Text); ++i)
+                    {
+                        //sw.Write("\t");
+                        for (int j = 1; j <= int.Parse(txtSoCot.Text); ++j)
+                        {
+                            arr[i, j] = ran.Next(1, 10);
+                            sw.Write(arr[i, j] + " ");
+                            sw1.Write(arr[i, j] + " ");
+                            
+                        }
+                        sw.WriteLine();
+                        sw1.WriteLine();
+                    }
+                    sw.Close();
+                    sw1.Close();
+                    MessageBox.Show("Input complete !");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            progressBar1.Invoke(new MethodInvoker(() =>
+            {
+                
+                progressBar1.Value = 0;
+            }));
+            try
+            {
+                using (StreamReader sr = new StreamReader("mydata.txt"))
+                {
+                   
+                    string line;
+                    line = sr.ReadToEnd();
+                    richTextBox1.Invoke(new MethodInvoker(() =>
+                    {
+                        richTextBox1.SelectionIndent = 20;
+                        richTextBox1.Text = line;
+                    }));
+                    //richTextBox1.SelectionIndent = 20;
+                    //richTextBox1.Text = line;
+
+                    sr.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Invoke(new MethodInvoker(() =>
+                {
+                    richTextBox1.Clear();
+                }));
+                Thread th1 = new Thread(new ThreadStart(inputMatrix));
+                th1.Start();
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void thoátToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Application.Exit();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Search()
+        {
+            try
+            {              
+                StreamReader sr = new StreamReader("mydata2.txt");
+                int number = 0;
+                string s = "";
+                int o;
+                s = sr.ReadToEnd();
+                int[,] a = new int[10000, 10000];
+
+                listBox1.Invoke(new MethodInvoker(() =>
+                {
+                    listBox1.Items.Clear();
+                }));
+                string[] text = s.Trim().Split(' ');
+                for (int i = 0; i < text.Length; ++i)
+                {
+                    o = int.Parse(text[i]);
+                    if (o == (int.Parse(txtK.Text)))
+                    {
+                        
+                        ++number;
+                        listBox1.Invoke(new MethodInvoker(() =>
+                        {
+                            listBox1.Items.Add("number+1 : " + number);
+                        }));
+                    }
+                }
+                MessageBox.Show("Số phần tử giống với K : " + number);
+
+                sr.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Thread th2 = new Thread(new ThreadStart(Search));
+                th2.Start();
+                //Search();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+  
+    }
+}
